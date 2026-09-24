fe1fd4ca07ef3f993a48ae59cf1865d10bd98021
Andreas Persson <andreas.persson@elva-group.com>
2026-09-24 11:33:47 +0200

    chore: authreach 1.0.3

--- Casks/authreach.rb
+++ Casks/authreach.rb
@@ -1,10 +1,10 @@
 cask "authreach" do
-  version "1.0.2"
-  sha256 "bbfe2fe7336e1e1411e94eb6eb148cdedb4abe3e6099c6a8d20d3a334022da6d"
+  version "1.0.3"
+  sha256 "2fdb039b43d0480585d8930ed9421e61c15c8aff7b958dfa0f66492b4d40836d"
 
   url "https://github.com/elva-labs/authreach/releases/download/v#{version}/authreach-#{version}.zip"
   name "AuthReach"
-  desc "Menu-bar app surfacing one-time passcodes from your Gmail inboxes"
+  desc "Menu-bar app surfacing one-time passcodes from your Gmail and IMAP inboxes"
   homepage "https://github.com/elva-labs/authreach"
 
   livecheck do
